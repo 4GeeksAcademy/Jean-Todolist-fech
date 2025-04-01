8531a363346ba4d1a6e48124bdb9ae1208e0f819
JeankenderJoseph3066 <jeankenderjoseph28@gmail.com>
2025-04-01 00:22:51 +0000

    done

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,37 +1,86 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("")
-	const [Todo, setTodo] = useState([])
-	
-	return (
+    const [InputValue, setInputValue] = useState("")
+    const [ToDo, setToDo] = useState([])
 
-		<div className="container mt-5">
-			<h1>Todo List</h1>
-			<ul>
-				<li>
-					<input
-				 type="text"
-				onChange={(e) => setInputValue(e.target.value)}
-				value={inputValue}
-				onKeyDown={(e) => {
-					if (e.key === "Enter") {
-                   setTodo(Todo.concat(inputValue));
-				   setInputValue("");
-					}
-				}}
-				placeholder="What do you need to do?"></input></li>
-				{Todo.map((item, index) => (
-                  <li>
-                 {item} {""} <span className="" onClick={() => setTodo(Todo.filter((t, currentIndex) => index != currentIndex))}>X</span>
-				  </li>
-				))}
-			</ul>
-			<div>{Todo.length} Item left</div>
-		</div>
-	);
+    useEffect(()=> {
+
+        getUser()
+    },[])
+	
+    // useEffect(() => {}, [ToDo])
+    const addToList = async (e) => {
+        e.preventDefault();
+        let ToDo = {label: InputValue, is_done: false}
+        let response = await fetch('https://playground.4geeks.com/todo/todos/jeankender', {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(ToDo)
+        })
+        let data = await response.json()
+        getUser()
+        setInputValue("")
+    };
+    const removeToDo = async (X) =>{
+        let deleteResponse = await fetch(`https://playground.4geeks.com/todo/todos/${X}`, {
+            method: "DELETE",
+            headers: { "Content-type": "application/json" },
+        })
+        getUser()
+    };
+
+
+    const getUser = async() => {
+        let response = await fetch('https://playground.4geeks.com/todo/users/jeankender')
+        let data = await response.json()
+        console.log(data)
+        if(typeof data.name !='undefined') {
+            setToDo(data.todos)
+            console.log(data.name)
+        }
+        else {
+            let response = await fetch('https://playground.4geeks.com/todo/users/jeankender',{
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+            })
+            let data = await response.json()
+            // console.log(data)
+        }
+    }
+
+
+
+
+
+
+return (
+
+	<div className="container mt-5">
+		<h1>Todo List</h1>
+		<ul>
+			<li>
+				<input
+					type="text"
+					onChange={(e) => setInputValue(e.target.value)}
+					value={InputValue}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							addToList(e)
+						}
+					}}
+					placeholder="What do you need to do?"></input></li>
+			{ToDo.map((item, index) => (
+				<li key ={item.id}>
+					{item.label} {""} <span className="" onClick={() => {removeToDo(item.id)}}>X</span>
+				</li>
+			))}
+		</ul>
+		<div>{ToDo.length} Item left</div>
+	</div>
+);
 };
 
 export default Home;
